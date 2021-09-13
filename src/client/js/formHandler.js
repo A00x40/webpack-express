c7c1d70b46ccd16d3be748a077ff3a9d0db2a57a
a00x40 <a00x401@gmail.com>
2021-09-13 07:49:31 +0200

    WebPack Set & Form Request to server/index.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,23 @@
-function handleSubmit(event) {
+export function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+
+    fetch('/api/test',{    
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            txt: formText
+        })
     })
+    .then(res => {
+        console.log(res)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,47 @@
 var path = require('path')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app_key = process.env.API_KEY
+
 const express = require('express')
-
 const app = express()
+app.use(express.json({
+    type: ['application/json', 'text/plain']
+}));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.use(express.static('src/client'))
+//const fetch = require('node-fetch')
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
+    res.sendFile('dist/index.html', { root: __dirname + '/..' })
+})
+
+app.post('/api/test', function (req, res) {
+    
+    const requestOptions = {
+        method: 'POST',
+        body: {
+            "txt": req.body.txt,
+            "key": process.env.API_KEY
+        },
+        redirect: 'follow'
+    };
+
+    //fetch("https://api.meaningcloud.com/lang-4.0/identification", requestOptions)
+    
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
 
-app.get('/test', function (req, res) {
-    console.log("Response")
-})
+
+
+
